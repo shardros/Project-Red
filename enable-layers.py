1a91d899ecde9345c471677ca9b5db8e19a70d05
shardros <shardros@gmail.com>
2018-07-08 19:45:49 +0000

    Backup from red server

---
+++ enable-layers.py
@@ -0,0 +1,23 @@
+#Import modules
+import RPi.GPIO  as GPIO
+import time
+import random
+
+#Set-up of modules
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+
+#Init globals 
+cube = [[[0 for k in xrange(4)] for j in xrange(4)] for i in xrange(4)]
+layer_pins = [29, 33, 31, 35] 
+collum_pins = [[10, 7, 26, 40], [11, 8, 38, 36], [13, 5, 32, 22], [3, 15, 24, 37]]
+
+frame_rate = 60
+
+
+for i in range(0,len(layer_pins)):
+	#Setup the layers and turn the pins high
+	GPIO.setup(layer_pins[i], GPIO.OUT)
+	GPIO.output(layer_pins[i], 1)
+	print("Setup pin ", str(layer_pins[i]), " as output and set it to one")
+
